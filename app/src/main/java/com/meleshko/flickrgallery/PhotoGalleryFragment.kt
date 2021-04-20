8a29f9352f24ea23430e03562751a126d1248f52
alex.M <meleha.alex@gmail.com>
2021-04-20 10:14:57 +0300

    Add ViewModel

--- app/src/main/java/com/meleshko/flickrgallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/meleshko/flickrgallery/PhotoGalleryFragment.kt
@@ -5,9 +5,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.meleshko.flickrgallery.api.GalleryItem
@@ -15,6 +16,7 @@ import com.meleshko.flickrgallery.api.GalleryItem
 class PhotoGalleryFragment : Fragment() {
 
     private lateinit var photoRecyclerView: RecyclerView
+    private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,17 +31,40 @@ class PhotoGalleryFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        photoGalleryViewModel = ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
+    }
 
-        val flickrLiveData: LiveData<List<GalleryItem>> = FlickrFetchr().fetchContent()
-        flickrLiveData.observe(
-            this,
-            Observer { responseString ->
-                Log.i("ZZZ", "Response received: $responseString")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        photoGalleryViewModel.galleryItemLiveData.observe(
+            viewLifecycleOwner,
+            Observer { galleryItems ->
+                photoRecyclerView.adapter = PhotoAdapter(galleryItems)
             }
         )
     }
 
     companion object {
         fun newInstance() = PhotoGalleryFragment()
+    }
+
+    private class PhotoHolder(itemTextView: TextView) : RecyclerView.ViewHolder(itemTextView) {
+        val bindTitle: (CharSequence) -> Unit = itemTextView::setText
+    }
+
+    private class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
+        RecyclerView.Adapter<PhotoHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
+            val textView = TextView(parent.context)
+            return PhotoHolder(textView)
+        }
+
+        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val galleryItem = galleryItems[position]
+            holder.bindTitle(galleryItem.title)
+        }
+
+        override fun getItemCount(): Int = galleryItems.size
+
     }
 }
---
+++ app/src/main/java/com/meleshko/flickrgallery/PhotoGalleryViewModel.kt
@@ -0,0 +1,14 @@
+package com.meleshko.flickrgallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.meleshko.flickrgallery.api.GalleryItem
+
+class PhotoGalleryViewModel : ViewModel() {
+
+    val galleryItemLiveData:  LiveData<List<GalleryItem>>
+
+    init {
+        galleryItemLiveData = FlickrFetchr().fetchContent()
+    }
+}
